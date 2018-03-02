8ce5092c31c7953f0811a3f06a7dcc327dbbbc98
Setia Anggraeni <tia.anggraeni2@gmail.com>
2018-03-02 16:30:57 +0700

    Exercise 3

---
+++ Exercise3.js
@@ -0,0 +1,19 @@
+var nama="Tia";
+var peran="Ksatria";
+
+
+if(nama =='') {
+  console.log("Nama harus diisi!"); 
+}
+else if(peran =='') {
+  console.log("Halo Mikael, Pilih peranmu untuk memulai game!"); 
+} 
+else if(peran =='Ksatria') {
+  console.log("Selamat datang di Dunia Proxytia, Nina" + "Halo Ksatria Nina, kamu dapat menyerang dengan senjatamu!"); 
+} 
+else if(peran =='Tabib') {
+  console.log("Selamat datang di Dunia Proxytia, Danu" + "Halo Tabib Danu, kamu akan membantu temanmu yang terluka."); 
+} 
+else if(peran =='Penyihir') {
+  console.log("Selamat datang di Dunia Proxytia, Zero" + "Halo Penyihir Zero, ciptakan keajaiban yang membantu kemenanganmu!"); 
+} 
