0ddc81b22aad8f91e7e04046afeb7a3d22df7997
Setia Anggraeni <tia.anggraeni2@gmail.com>
2018-03-04 21:26:00 +0700

    Exercise7

---
+++ Exercise7.js
@@ -0,0 +1,18 @@
+//1. Menyusun Barisan Bintang
+for (var baris= 0; baris < 5; baris++) { 
+    console.log('*');
+  }
+  
+  //2. Menyusun Barisan Bintang Dengan Nested Looping
+  for (var baris= 0; baris < 5; baris++) { 
+    console.log('*****');
+  }
+  
+  
+  //3. Menyusun Barisan Tangga Bintang Dengan Nested Looping
+  var baris='';
+  for (var i= 0; i < 5; i++) { 
+    baris += '*';
+    console.log(baris);
+  }
+  
