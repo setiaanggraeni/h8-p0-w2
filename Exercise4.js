55072a6b258fd5425f18ed383d274e8df4096c78
Setia Anggraeni <tia.anggraeni2@gmail.com>
2018-03-04 21:20:43 +0700

    exercise4

---
+++ Exercise4.js
@@ -0,0 +1,18 @@
+var tanggal = 1;
+var bulan= 12;
+var tahun = 2018;
+switch(bulan) {
+  case 1:   { console.log(tanggal + " " + 'January' + " " + tahun); break; }
+  case 2:   { console.log(tanggal + " " + 'February' + " " + tahun); break; }
+   case 3:   { console.log(tanggal + " " + 'March' + " " + tahun); break; }
+  case 4:   { console.log(tanggal + " " + 'April' + " " + tahun); break; }
+    case 5:   { console.log(tanggal + " " + 'May' + " " + tahun); break; }
+  case 6:   { console.log(tanggal + " " + 'June' + " " + tahun); break; }
+    case 7:   { console.log(tanggal + " " + 'July' + " " + tahun); break; }
+  case 8:   { console.log(tanggal + " " + 'August' + " " + tahun); break; }
+    case 9:   { console.log(tanggal + " " + 'September' + " " + tahun); break; }
+  case 10:   { console.log(tanggal + " " + 'October' + " " + tahun); break; }
+    case 11:   { console.log(tanggal + " " + 'November' + " " + tahun); break; }
+  case 12:   { console.log(tanggal + " " + 'December' + " " + tahun); break; }
+  default:  { console.log('Salah Input Bulan'); }
+}
