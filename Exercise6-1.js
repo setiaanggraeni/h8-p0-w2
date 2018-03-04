236cd2f7bf89af1823093287cb4186064dae8c50
Setia Anggraeni <tia.anggraeni2@gmail.com>
2018-03-04 21:24:03 +0700

    Exercise6

---
+++ Exercise6-1.js
@@ -0,0 +1,12 @@
+var coding = 0;
+console.log ('LOOPING PERTAMA');
+while(coding < 20) { 
+  coding += 2; 
+  console.log(coding + ' - I love coding');
+}
+var developer = 22;
+console.log ('LOOPING KEDUA');
+while(developer > 2){
+  developer -= 2;
+  console.log(developer + ' I will become fullstack developer');
+}
